e19681c1205eac944e1bce5bd927be2eb18a5cbc
count-fuzzball <50670715+count-fuzzball@users.noreply.github.com>
2020-01-07 12:13:42 +0000

    Initial Commit
    First upload of my script that pulls DDS files from the contents of PFXTS files.

---
+++ MGSV GetDDS Example/abcde_pxfts/GetDDS.ps1
@@ -0,0 +1,15 @@
+﻿#Get all .ftex names in the current folder and sub-folders.
+$pftxsfolderitems = Get-ChildItem ".\" -Attributes !Directory+!System -recurse -Exclude *.1.ftexs,*.2.ftexs,*.3.ftexs,*.ps1
+
+#Path to unpacked DDS files.
+$PathToDDS = "..\texturesconverted"
+
+foreach ($i in $pftxsfolderitems) 
+{
+ $BasenameFtexs = $i.BaseName
+ #$FullPathToFilename = $i.FullName
+
+ mkdir -force DDS_Files
+
+ copy-item "$PathToDDS\$BasenameFtexs.dds" "DDS_Files\$BasenameFtexs.dds"
+}
