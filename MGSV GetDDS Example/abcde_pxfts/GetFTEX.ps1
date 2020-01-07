ae8284ce0b04a3fff6e8140cc09c67e4d5b48902
count-fuzzball <50670715+count-fuzzball@users.noreply.github.com>
2020-01-07 14:51:36 +0000

    Added an Ftex "collater"
    Tries to get all Ftex, .1.ftexs, .2.ftexs, etc files together from your unpacked MGSV archives folder.

---
+++ MGSV GetDDS Example/abcde_pxfts/GetFTEX.ps1
@@ -0,0 +1,17 @@
+﻿#Get all .ftex names in the current folder and sub-folders.
+$pftxsfolderitems = Get-ChildItem ".\" -Attributes !Directory+!System -recurse -Exclude *.ps1 *.ftex *.*.ftexs
+
+#Path to unpacked DDS files.
+$PathToFtex = "..\"
+
+foreach ($i in $pftxsfolderitems)
+{
+ $BasenameFtexs = $i.BaseName
+ #$FullPathToFilename = $i.FullName
+
+ mkdir -force Ftexs_Files
+
+ get-childitem -path "$PathToFtex" -filter "$BasenameFtexs.*.ftexs" | copy-item -destination "Ftexs_Files\"
+ copy-item "$BasenameFtexs.ftex" -Destination "Ftexs_Files\$BasenameFtexs.ftex"
+ copy-item "$BasenameFtexs.1.ftexs" -Destination "Ftexs_Files\$BasenameFtexs.1.ftexs"
+}
